4c178e4fcef8614b7494a50ba5845be4bb062466
guilherme <guilherme.lamim96@gmail.com>
2023-02-16 16:30:03 -0300

    criado componente GameCard

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -4,6 +4,8 @@ import logoImg from "../../assets/logo-nlw-esports.png";
 
 import { styles } from "./styles";
 import { Heading } from "../../components/Heading";
+import { GameCard } from "../../components/GameCard";
+import { GAMES } from "../../utils/games";
 
 export function Home() {
   return (
@@ -11,6 +13,7 @@ export function Home() {
       <Image source={logoImg} style={styles.logo} />
 
       <Heading title="Encontre seu duo" subtitle="Selecione o game que deseja jogar..." />
+      <GameCard data={GAMES[0]}/>
     </View>
   );
 }
