2b752fdf6556c826071f9d375a06b1fabe4f3627
guilherme <guilherme.lamim96@gmail.com>
2023-02-16 09:13:53 -0300

    criado componente header na home

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import { View } from 'react-native';
+import React from "react";
+import { View, Image } from "react-native";
+import logoImg from "../../assets/logo-nlw-esports.png";
 
-import { styles } from './styles';
+import { styles } from "./styles";
+import { Heading } from "../../components/Heading";
 
 export function Home() {
   return (
     <View style={styles.container}>
+      <Image source={logoImg} style={styles.logo} />
 
+      <Heading title="Encontre seu duo" subtitle="Selecione o game que deseja jogar..." />
     </View>
   );
 }
